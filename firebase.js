55eb67bd5ed5d747bd86c6e6052343fc00c70d25
parvinder <parvinderbabar@gmail.com>
2024-08-20 14:20:17 -0400

    change firebae config

--- firebase.js
+++ firebase.js
@@ -4,14 +4,7 @@
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
 // // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBRQbnV5cYhdqaNoG2HLzAWEGyxLze4A5U",
-//   authDomain: "flashcard-saas-89d6a.firebaseapp.com",
-//   projectId: "flashcard-saas-89d6a",
-//   storageBucket: "flashcard-saas-89d6a.appspot.com",
-//   messagingSenderId: "123965136964",
-//   appId: "1:123965136964:web:b2b344f9c331b4621e5c11"
-// };
+
 
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
@@ -23,14 +16,22 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAwfaWtI5vexH-58ejak2dRojCo-tV3ey4",
+//   authDomain: "flashcard-40fea.firebaseapp.com",
+//   projectId: "flashcard-40fea",
+//   storageBucket: "flashcard-40fea.appspot.com",
+//   messagingSenderId: "232774355145",
+//   appId: "1:232774355145:web:c4aa979a15e14b1b89a518",
+//   measurementId: "G-Q5QZ1XGHTS"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyAwfaWtI5vexH-58ejak2dRojCo-tV3ey4",
-  authDomain: "flashcard-40fea.firebaseapp.com",
-  projectId: "flashcard-40fea",
-  storageBucket: "flashcard-40fea.appspot.com",
-  messagingSenderId: "232774355145",
-  appId: "1:232774355145:web:c4aa979a15e14b1b89a518",
-  measurementId: "G-Q5QZ1XGHTS"
+  apiKey: "AIzaSyBRQbnV5cYhdqaNoG2HLzAWEGyxLze4A5U",
+  authDomain: "flashcard-saas-89d6a.firebaseapp.com",
+  projectId: "flashcard-saas-89d6a",
+  storageBucket: "flashcard-saas-89d6a.appspot.com",
+  messagingSenderId: "123965136964",
+  appId: "1:123965136964:web:b2b344f9c331b4621e5c11"
 };
 
 // Initialize Firebase
